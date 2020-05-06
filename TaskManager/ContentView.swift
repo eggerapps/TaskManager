1e45b2ecc10798a01bbf5d333c46feae3cb027d5
Jakob <jakob@eggerapps.at>
2020-05-06 18:13:39 +0200

    Initial Commit

---
+++ TaskManager/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TaskManager
+//
+//  Created by Jakob Egger on 2020-05-06.
+//  Copyright © 2020 eggerapps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
